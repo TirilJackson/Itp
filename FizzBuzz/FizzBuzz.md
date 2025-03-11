866baf0a436e7924ad41d25703d2785226ae7d3c
Tiril Jackson <Tirilljackson@outlook.com>
2025-03-11 12:40:56 -0400

    rachel fixed file structure

---
+++ FizzBuzz/FizzBuzz.md
@@ -0,0 +1,6 @@
+# this is a header, rachel!
+
+This was the answer I got 
+I used chat gpt to ask for the code. I tried folloing the code along but keept getting errors 
+ended up trying this code and it worked 
+I copied the code along asn asked for a code that followed the instructions based of the code along examples. 
---
+++ FizzBuzz/fizzbuzz.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 100; i++) {
+  console.log(`The next MIDI note value is ${i}`);
+}
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
